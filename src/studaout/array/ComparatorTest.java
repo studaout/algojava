027854256e0024373fadd8b2544a54ce3bb3770b
studaout <studaout@gmail.com>
2016-12-22 22:57:44 -0500

    Container With Most Water

---
+++ src/studaout/array/ComparatorTest.java
@@ -0,0 +1,27 @@
+package studaout.array;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Created by timout on 12/8/14.
+ */
+public class ComparatorTest {
+
+    public static void main(String[] args) {
+        Integer arr[] = {1,2,3,4,5,6,7,8,9,10};
+        List<Integer> l = Arrays.asList(arr);
+        l.sort((Integer a, Integer b) -> (a > b) ? 1 : (a.equals(b) ? 0 : -1));
+        System.out.println("natural: " + l);
+        l = Arrays.asList(arr);
+        l.sort((Integer a, Integer b) -> (a < b) ? 1 : (a.equals(b) ? 0 : -1));
+        System.out.println("reverse: " + l);
+        l = Arrays.asList(arr);
+        l.sort((Integer a, Integer b) -> 1);
+        System.out.println("reverse: " + l);
+        l.sort((Integer a, Integer b) -> -1);
+        System.out.println("reverse: " + l);
+    }
+}
